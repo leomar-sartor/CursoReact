ebe6d76bab2cbfadf1c54f4577772defaf226035
Leomar <leomar.sartor@funcionalcorp.com.br>
2023-06-01 16:48:03 -0300

    feat: add material ui

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,1 +1,12 @@
-export {};
+import { Button } from "@mui/material";
+import { Route, Routes, Navigate } from "react-router-dom";
+
+export const AppRoutes = () => {
+  return (
+    <Routes>
+        <Route path="/home" element={<Button>First Button</Button>} />
+
+        <Route path="*" element={<Navigate to="/home"/>} />
+    </Routes>
+  );
+};

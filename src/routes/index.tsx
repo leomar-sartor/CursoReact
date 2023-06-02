7c65c2ec6b892552884c5481515d9e029cef0452
Leomar <leomar.sartor@funcionalcorp.com.br>
2023-06-02 11:26:15 -0300

    feat: add eslint

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,12 +3,18 @@ import { Route, Routes, Navigate } from "react-router-dom";
 import { useAppThemeContext } from "../shared/contexts";
 
 export const AppRoutes = () => {
-
   const { toggleTheme } = useAppThemeContext();
 
   return (
     <Routes>
-      <Route path="/home" element={<Button variant="contained" color='primary' onClick={toggleTheme}>First Button</Button>} />
+      <Route
+        path="/home"
+        element={
+          <Button variant="contained" color="primary" onClick={toggleTheme}>
+            First Button
+          </Button>
+        }
+      />
 
       <Route path="*" element={<Navigate to="/home" />} />
     </Routes>
